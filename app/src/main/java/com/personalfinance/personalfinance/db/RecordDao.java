c3595af4fe84146979e312ad7e0aed2369a4aae6
TAY TS <taytzushieh@gmail.com>
2019-01-14 20:43:27 +0800

    feature: Add the DAO for Record table

---
+++ app/src/main/java/com/personalfinance/personalfinance/db/RecordDao.java
@@ -0,0 +1,48 @@
+package com.personalfinance.personalfinance.db;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.TypeConverter;
+import android.arch.persistence.room.TypeConverters;
+import android.arch.persistence.room.Update;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
+import static android.arch.persistence.room.OnConflictStrategy.IGNORE;
+import static android.arch.persistence.room.OnConflictStrategy.REPLACE;
+
+@Dao
+@TypeConverters({BigDecimalConverter.class, DateConverter.class})
+public interface RecordDao {
+
+    // Insert new record
+    @Insert(onConflict = IGNORE)
+    void insert(Record record);
+
+    // Get specific record by ID
+    @Query("SELECT * FROM RECORD WHERE ID = :id")
+    Record getRecordById(int id);
+
+    // Get a list of records by range
+    @Query("SELECT * FROM RECORD " +
+            "WHERE (RECORD_TYPE = :recordType) AND CREATE_TIMESTAMP BETWEEN :startDate AND :endDate " +
+            "ORDER BY CREATE_TIMESTAMP ASC")
+    LiveData<List<Record>> getRecordByRange(int recordType, Date startDate, Date endDate);
+
+    // Update record
+    @Update(onConflict = REPLACE)
+    void updateRecord(Record record);
+
+    // Delete record by Object
+    @Delete
+    int deleteRecord(Record record);
+
+    // Delete record by ID
+    @Query("DELETE FROM RECORD WHERE ID = :id")
+    int deleteRecord(int id);
+}
