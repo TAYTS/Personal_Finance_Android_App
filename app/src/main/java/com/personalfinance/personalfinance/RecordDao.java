3db7edb106aae26e4e6d0e66d0ac99437325fa95
TAY TS <taytzushieh@gmail.com>
2019-01-17 23:27:41 +0800

    chore: Remove deprecated and unused library

--- app/src/main/java/com/personalfinance/personalfinance/RecordDao.java
+++ app/src/main/java/com/personalfinance/personalfinance/RecordDao.java
@@ -1,17 +1,13 @@
 package com.personalfinance.personalfinance;
 
 import android.arch.lifecycle.LiveData;
-import android.arch.lifecycle.MutableLiveData;
-import android.arch.paging.LivePagedListProvider;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
-import android.arch.persistence.room.TypeConverters;
 import android.arch.persistence.room.Update;
 
 import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 
 import static android.arch.persistence.room.OnConflictStrategy.IGNORE;
